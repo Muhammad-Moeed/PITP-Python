3cd340d814578ed6a1c20052834f776647565681
Muhammad Moeed <m.moeedq497@gmail.com>
2025-01-17 17:17:04 +0500

    class5 code
    #Compriison Operator
    #Logocal (And or Not) Opearor
    # Indentity operator(is or is not)
    # Membership Opearator(in or innot)

---
+++ class5.py
@@ -0,0 +1,85 @@
+#Compriison Operator
+#Logocal (And or Not) Opearor
+# Indentity operator(is or is not)
+# Membership Opearator(in or innot)
+
+# Task 1  Formula : (a+b)**2
+
+# a = int(input("Enter a"))
+# b = int(input("Enter a"))
+
+# x = (a+b)**2
+# y = ((a**2) + (2*(a*b)) + b**2)
+
+# print("a is", + x)
+# print("b is", +y)
+
+
+# ***************Quadric Equatuin Solve ******************
+
+# a = int(input('Enter a'))
+# b = int(input('Enter b'))
+# c = int(input('Enter c'))
+
+# solveUnderRoot = (((b**2) - (4*a*c))**(0.5))
+
+# Result = (-b + solveUnderRoot) // 2*a
+
+# print(Result)
+
+# Task 3
+# t = ((a+3)/b)*4+c
+# print("((a+3)/b)*4+c = ", t)
+
+
+# #Indentity Operator
+# first = 'Hello'
+# second = 'Moeed'
+# if first is second:
+#     print("ha")
+# else:
+#     print("nhi")
+
+# #MemberShip Operator
+# if first in second:
+#     print("ha")
+# else:
+#     print("nhi")
+
+# BitWise Operator
+#AND
+# if 2>1 & 6<8:
+#     print("yes")
+# else:
+#     print('No')
+
+# # OR
+# userage = int(input("Enter your age"))
+# age =18
+# if  userage > age or  userage == age:
+#     print("You are Elligible")
+# else:
+#     print("Not elligible " )
+
+#NOT
+userage = int(input("Enter your age"))
+age =18
+if  not(userage > age and userage == age):
+    print("You not Elligible")
+else:
+    print("You are Elligible")
+
+
+# XOR 
+# if 2>1 ^ 6<8:
+#     print("yes")
+# else:
+#     print('No')
+
+# formula =((a**2)+(3*(a)*(b)))/(2*(c**2))**(0.5)
+# forrResult =( formula)+ ((b)/(c+2))
+# print(forrResult)
+
+
+
+
