3bdfa12ba02093bf902ca63fe23a5257583904dd
Muhammad Moeed <m.moeedq497@gmail.com>
2025-02-11 00:09:22 +0500

    recursive update

--- class18.py
+++ class18.py
@@ -28,6 +28,36 @@ sum_list([1, 2, 3, 4, 5])
 
 # Develop a function to calculate n numbers of Fibonanci series using recursion
 # n should be taken from user input 
+#Recursion
+
+# Define a recursive function to calculate the factorial of a number
+def recur(i):
+    if i == 0:
+        return 1
+    else:
+        return i * recur(i-1)
+
+recur(5)  
+
+# Sum of Natural Numbers
+def facsum(j):
+    if j == 0:
+        return 1
+    else:
+        return j + recur(j-1)
+facsum(5)
+
+# Develop a function to calculate the sum of values within a list using recursion
+def sum_list(lst):
+    if len(lst) == 0:
+        return 0
+    else:
+        return lst[0] + sum_list(lst[1:])
+
+sum_list([1, 2, 3, 4, 5])
+
+# Develop a function to calculate n numbers of Fibonanci series using recursion
+# n should be taken from user input 
 def fibonacci(n):
     if n <= 0:
         return []
